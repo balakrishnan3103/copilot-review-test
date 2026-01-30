d3db4683270b1576e36cb28d9674472c31ac0da5
balakrishnan <balakrishnan.ashokkumar@agilysys.com>
2026-01-30 18:47:44 +0530

    removed comment

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -80,7 +80,7 @@ public class Calculator {
     }
 
     // ISSUE 7: Empty catch block
-    public int parseNumber(String str) {
+    public int parseNumberss(String str) {
         try {
             return Integer.parseInt(str);
         } catch (NumberFormatException e) {
